4b9324bb63233923748795757507b870e1d6f3d9
javierlnc <javierlnc@icloud.com>
2025-01-06 01:50:03 -0500

    Update app layout and components for Sneakers App; enhance login page styling and add logo

--- frontend/components/common/Navbar.tsx
+++ frontend/components/common/Navbar.tsx
@@ -1,9 +1,23 @@
-import { NextPage } from 'next'
-
-
+import { NextPage } from "next";
+import Image from "next/image";
 
 const Navbar: NextPage = ({}) => {
-  return <div>Navbar</div>
-}
+  return (
+    <header className="w-full  absolute z-10 ">
+      <nav className="max-w-[1440px] mx-auto flex justify-between sm:px-16 px-6 py-4">
+        <div>
+          <Image
+            src="/logo.svg"
+            alt="sneakers logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+        </div>
+        
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
