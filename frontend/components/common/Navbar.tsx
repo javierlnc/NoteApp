1537a192599770d8213fd91af918b1167dcfad2d
javierlnc <javierlnc@icloud.com>
2025-01-06 02:43:49 -0500

    Add SVG icons for menu and notes; enhance Navbar with dropdown functionality

--- frontend/components/common/Navbar.tsx
+++ frontend/components/common/Navbar.tsx
@@ -1,20 +1,61 @@
 import { NextPage } from "next";
 import Image from "next/image";
+import Link from "next/link";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 
 const Navbar: NextPage = ({}) => {
   return (
     <header className="w-full  absolute z-10 ">
-      <nav className="max-w-[1440px] mx-auto flex justify-between sm:px-16 px-6 py-4">
+      <nav className="max-w-[1440px] border-b-2 border-grayish mx-auto flex justify-between sm:px-16 px-6 py-4">
+        <Menu>
+          <MenuButton
+            className={"md:hidden inline-flex items-center gap-2 rounded-md"}
+          >
+            <Image src={"/icon-menu.svg"} alt="menu" width={24} height={24} />
+          </MenuButton>
+          <MenuItems
+          transition
+          anchor="bottom end"
+            className={
+              "flex flex-col w-40 min-h-screen origin-right rounded-lg shadow-lg p-1 text-sm/6 transition duration-500 ease-out [--anchor-gap:var(--spacing-1)]"
+            }
+          >
+            <MenuItem>
+              <Link href={"/"}>Collections</Link>
+            </MenuItem>
+            <MenuItem>
+              <Link href={"/"}>Men</Link>
+            </MenuItem>
+            <MenuItem>
+              <Link href={"/"}>Women</Link>
+            </MenuItem>
+            <MenuItem>
+              <Link href={"/"}>About</Link>
+            </MenuItem>
+            <MenuItem>
+              <Link href={"/"}>Contac</Link>
+            </MenuItem>
+          </MenuItems>
+        </Menu>
         <div>
           <Image
-            src="/logo.svg"
-            alt="sneakers logo"
-            width={118}
-            height={18}
+            src="/icon-notes.svg"
+            alt="Notes logo"
+            width={40}
+            height={40}
             className="object-contain"
           />
         </div>
-        
+        <div className="hidden md:flex">
+          <Link href={"/"}>Collections</Link>
+          <Link href={"/"}>Men</Link>
+          <Link href={"/"}>Women</Link>
+          <Link href={"/"}>About</Link>
+          <Link href={"/"}>Contac</Link>
+        </div>
+        <div>
+
+        </div>
       </nav>
     </header>
   );
