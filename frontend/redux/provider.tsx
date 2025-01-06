9fd859daf960019d2a6d63070c40e4582e2fcb02
javierlnc <javierlnc@icloud.com>
2025-01-06 05:11:38 -0500

    Add CustomProvider component for Redux store integration

---
+++ frontend/redux/provider.tsx
@@ -0,0 +1,11 @@
+'user client'
+import React from "react";
+import { store } from "./store";
+import { Provider } from "react-redux";
+
+interface Props {
+    children : React.ReactNode;
+}
+export default function CustomProvide({children}: Props){
+    return <Provider store={store}>{children}</Provider>
+}
