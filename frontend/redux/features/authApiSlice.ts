7a0548a699a3e518f71e3b85619405a89fbdb9f7
javierlnc <javierlnc@icloud.com>
2025-01-06 07:07:56 -0500

    Add authApiSlice for user authentication endpoints and update apiSlice import

---
+++ frontend/redux/features/authApiSlice.ts
@@ -0,0 +1,46 @@
+import { apiSlice } from "../services/apiSlice";
+interface User{
+    first_name: string;
+	last_name: string;
+	email: string;
+}
+const authApiSlice = apiSlice.injectEndpoints({
+    endpoints: builder => ({
+		retrieveUser: builder.query<User, void>({
+			query: () => '/users/me/',
+		}),
+        login: builder.mutation({
+			query: ({ email, password }) => ({
+				url: '/jwt/create/',
+				method: 'POST',
+				body: { email, password },
+			}),
+		}),
+		register: builder.mutation({
+			query: ({
+				first_name,
+				last_name,
+				email,
+				password,
+				re_password,
+			}) => ({
+				url: '/users/',
+				method: 'POST',
+				body: { first_name, last_name, email, password, re_password },
+			}),
+		}),
+        logout: builder.mutation({
+			query: () => ({
+				url: '/logout/',
+				method: 'POST',
+			}),
+		}),
+    })
+})
+export const {
+    useRetrieveUserQuery,
+    useLoginMutation,
+    useRegisterMutation,
+    useLogoutMutation
+
+} = authApiSlice;
--- frontend/redux/services/apiSlice.ts
+++ frontend/redux/services/apiSlice.ts
@@ -1,4 +1,4 @@
-import { createApi fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type {
   BaseQueryFn,
   FetchArgs,
@@ -52,5 +52,5 @@ const baseQueryWithReauth: BaseQueryFn<
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: baseQueryWithReauth,
-    endpoints: builder => ({})
+    endpoints: (builder) => ({}),
 })
