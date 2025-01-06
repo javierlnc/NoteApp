863ea95ffc5018d00572c37aec6b4e31a631afb7
javierlnc <javierlnc@icloud.com>
2025-01-06 06:19:56 -0500

    Add async-mutex for handling API token refresh and create apiSlice for Redux integration

--- frontend/redux/store.ts
+++ frontend/redux/store.ts
@@ -1,7 +1,9 @@
 import { configureStore} from "@reduxjs/toolkit";
+import { apiSlice } from "./services/apiSlice";
 import authReducer from './features/authSlice'
 export const store = configureStore({
     reducer:{
+        [apiSlice.reducerPath]: apiSlice.reducer,
        auth: authReducer
     },
     devTools: process.env.NODE_ENV !== 'production'
