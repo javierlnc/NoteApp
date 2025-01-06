ccedcb2254b650097b4be2785f967ad76b55aced
javierlnc <javierlnc@icloud.com>
2025-01-06 07:12:15 -0500

    Add middleware for apiSlice to Redux store configuration

--- frontend/redux/store.ts
+++ frontend/redux/store.ts
@@ -6,6 +6,8 @@ export const store = configureStore({
         [apiSlice.reducerPath]: apiSlice.reducer,
        auth: authReducer
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: process.env.NODE_ENV !== 'production'
 
 })
