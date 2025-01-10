ef050bfc230051d54359bf0cf053bdb4d2452a65
javierlnc <javierlnc@icloud.com>
2025-01-10 02:53:16 -0500

    Refactor user model to implement custom user manager and enhance user authentication; update settings for REST framework

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,22 +1,61 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
 # Create your models here.
 
-class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=100, unique=True)
+
+class MyUserManger(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+
+        if not email:
+            raise ValueError("Users must have an email address")
+        email = self.normalize_email(email)
+        email = email.lower()
+
+        user = self.model(
+            email=email,
+            **kwargs
+
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **kwargs):
+
+        user = self.create_user(
+            email,
+            password=password,
+            **kwargs
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, max_length=255)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superiser = models.BooleanField(default=False)
+    objects = MyUserManger()
     REQUIRED_FIELDS = ['first_name', 'last_name',]
     USERNAME_FIELD = 'email'
-    
+
     def __str__(self):
-        return self.numberid
-    
+        return self.email
+
+
 class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='notes')
-    
+    author = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='notes')
+
     def __str__(self):
         return self.title
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,11 +32,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-REST_FRAMEWORK={
-    "DEFAULT_AUTHENTICATION_CLASSES":(
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    "DEFAULT_PERMISSION_CLASSES":[
+    "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
 }
