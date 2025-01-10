299e4bab997f04b373dadbe542fac3e0a4ff3599
javierlnc <javierlnc@icloud.com>
2025-01-09 19:40:31 -0500

    Enhance registration page layout with improved image size and centered heading

--- frontend/app/auth/register/Page.tsx
+++ frontend/app/auth/register/Page.tsx
@@ -2,15 +2,17 @@ import Image from "next/image";
 export default function Page() {
   return (
     <div className="flex min-h-full flex-1 flex-col px-6 py-12 lg:px-8">
-      <div>
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <Image
           src="/icon-notes.svg"
           alt="Notes logo"
-          width={30}
-          height={30}
-          className="object-contain"
+          width={50}
+          height={50}
+          className="object-contain mx-auto"
         />
-        <h2></h2>
+        <h2 className="mt-10 text-center text-2xl font-bold tracking-thight text-gray-800">
+            Sign up for your account
+        </h2>
       </div>
 
       <div></div>
