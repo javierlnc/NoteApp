67d4a2d06c1eacd810771c07efc0771fdb3db39c
javierlnc <javierlnc@icloud.com>
2025-01-09 21:04:41 -0500

    Add registration form fields and update styles for improved user experience

--- frontend/app/auth/register/Page.tsx
+++ frontend/app/auth/register/Page.tsx
@@ -1,5 +1,22 @@
+"use client";
 import Image from "next/image";
+import { useState, ChangeEvent } from "react";
 export default function Page() {
+
+    const [formData, setFormData] = useState({
+        first_name : '',
+        last_name : '',
+        email : '',
+        password : '',
+        re_password : '',
+    });
+
+    const {first_name, last_name, email, password, re_password}= formData
+
+    const onChange = (event : ChangeEvent<HTMLInputElement>) =>{
+
+        setFormData({...formData, [event.target.name]: event.target.value})
+    }
   return (
     <div className="flex min-h-full flex-1 flex-col px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -20,15 +37,17 @@ export default function Page() {
           <div className="relative z-0 w-full mb-5 group">
             <input
               type="email"
-              name="floating_email"
-              id="floating_email"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              name="email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              className="form__input-field peer"
               placeholder=" "
               required
             />
             <label
-              htmlFor="floating_email"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              htmlFor="email"
+              className="form__label"
             >
               Email address
             </label>
@@ -36,15 +55,17 @@ export default function Page() {
           <div className="relative z-0 w-full mb-5 group">
             <input
               type="password"
-              name="floating_password"
-              id="floating_password"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              name="password"
+              id="password"
+              value={password}
+              onChange={onChange}
+              className="form__input-field peer"
               placeholder=" "
               required
             />
             <label
-              htmlFor="floating_password"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              htmlFor="password"
+              className="form__label"
             >
               Password
             </label>
@@ -52,15 +73,17 @@ export default function Page() {
           <div className="relative z-0 w-full mb-5 group">
             <input
               type="password"
-              name="repeat_password"
-              id="floating_repeat_password"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              name="re_password"
+              id="re_password"
+              value={re_password}
+              onChange={onChange}
+              className="form__input-field peer"
               placeholder=" "
               required
             />
             <label
-              htmlFor="floating_repeat_password"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              htmlFor="re_password"
+              className="form__label"
             >
               Confirm password
             </label>
@@ -69,15 +92,17 @@ export default function Page() {
             <div className="relative z-0 w-full mb-5 group">
               <input
                 type="text"
-                name="floating_first_name"
-                id="floating_first_name"
-                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                name="first_name"
+                id="first_name"
+                value={first_name}
+                onChange={onChange}
+                className="form__input-field peer"
                 placeholder=" "
                 required
               />
               <label
-                htmlFor="floating_first_name"
-                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                htmlFor="first_name"
+                className="form__label"
               >
                 First name
               </label>
@@ -85,15 +110,17 @@ export default function Page() {
             <div className="relative z-0 w-full mb-5 group">
               <input
                 type="text"
-                name="floating_last_name"
-                id="floating_last_name"
-                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                name="last_name"
+                id="last_name"
+                value={last_name}
+                onChange={onChange}
+                className="form__input-field peer"
                 placeholder=" "
                 required
               />
               <label
-                htmlFor="floating_last_name"
-                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                htmlFor="last_name"
+                className="form__label"
               >
                 Last name
               </label>
